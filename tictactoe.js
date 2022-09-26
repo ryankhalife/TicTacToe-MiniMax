f163d3eb6dec8dcf0fa32c19b2f73647dd6c3279
Ryan Khalife <khaliferyan@gmail.com>
2022-09-26 22:31:09 +0300

    highlight winning combination on gameover

--- tictactoe.js
+++ tictactoe.js
@@ -1,5 +1,3 @@
-// Red: 1; Blue: 2; Red start
-
 const grid = Array(9).fill(0);
 const player = "red";
 const winning_combinations = [
@@ -16,43 +14,46 @@ let whose_turn = "red";
 let move_played = 0;
 let game_over = 0;
 
-const cells = document.querySelectorAll(".row div");
-
 const handleClick = (e) => {
   const move = e.target.closest(".row div").id[1];
   if (grid[move] || whose_turn != player || game_over) return;
-
   playMove(move);
-  playRandomMove();
 };
 
 const playMove = (move) => {
-  if (move_played > 9 || game_over) return;
+  if (game_over) return;
 
   grid[move] = whose_turn;
   document.getElementById(`c${move}`).classList.add(whose_turn);
   whose_turn = whose_turn == "red" ? "yellow" : "red";
   move_played++;
   checkWinners();
+
+  if (move_played < 9 && whose_turn != player) playRandomMove();
 };
 
 const playRandomMove = () => {
-  if (game_over || move_played > 9) return;
+  if (game_over) return;
   let move = Math.floor(Math.random() * 9);
   while (grid[move]) move = Math.floor(Math.random() * 9);
   playMove(move);
 };
 
 const checkWinners = () => {
-  winning_combinations.forEach((cmb) => {
-    if (grid[cmb[0]] != 0 && grid[cmb[0]] == grid[cmb[1]] && grid[cmb[0]] == grid[cmb[2]]) {
-      game_over = 1;
-      cmb.forEach((nb) => {
-        document.getElementById(`c${nb}`).style.backgroundColor = grid[nb];
+  winning_combinations.forEach((wc) => {
+    if (grid[wc[0]] && grid[wc[0]] == grid[wc[1]] && grid[wc[0]] == grid[wc[2]]) {
+      game_over = 1; // Red/Yellow win
+      cells.forEach((cell) => {
+        if (!wc.includes(parseInt(cell.id[1]))) cell.classList.add("brightness-low");
       });
       return;
     }
   });
+  if (!game_over && move_played >= 9) {
+    game_over = 1;
+    cells.forEach((cell) => cell.classList.add("brightness-low"));
+  }
 };
 
+const cells = document.querySelectorAll(".row div");
 cells.forEach((cell) => cell.addEventListener("click", handleClick));
